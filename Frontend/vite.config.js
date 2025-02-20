cc73a72b34bc4be3737349da257a8b8a4cef3268
PRITISH TOMAR <pritishtomar2004@gmail.com>
2025-02-21 02:40:55 +0530

    Fixing prod. build

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // For proper asset linking in serve
-  build: {
-    outDir: 'dist',
+  base: './',
+  optimizeDeps: {
+    include: ['@material-tailwind/react'],
   },
 });
