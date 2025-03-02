afc089d72457f5af42de524080cd35986221409b
PRITISH TOMAR <pritishtomar2004@gmail.com>
2025-03-02 23:13:41 +0530

    added bg-vid/aditi

--- Frontend/src/Components/homepage/hero.jsx
+++ Frontend/src/Components/homepage/hero.jsx
@@ -2,22 +2,40 @@ import React from "react";
 
 // assets
 import heroLogoSrc from "../../assets/Landing/White.png";
-import bgImgSrc from "../../assets/Landing/MIsd.png";
+import Vdo from "../../assets/Landing/vdo.mp4"; // Import the video
+import smallLogoSrc from "../../assets/Landing/mmmut.jpg"; // Import the small logo
+import small from "../../assets/Logo-images/Abhyudaya.png";
 
 function HeroSection() {
   return (
     <div className="relative w-full h-screen grid place-items-center">
-      <div
-        className="absolute grid place-items-center h-full w-full overflow-hidden"
-        style={{
-          backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.36)), url(${bgImgSrc})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="grid place-items-center overflow-hidden">
-          <img className="max-w-full h-auto" src={heroLogoSrc} alt="Abhyudaya Logo" />
+      <div className="absolute grid place-items-center h-full w-full overflow-hidden">
+        <video
+          className="absolute w-full h-full object-cover"
+          src={Vdo}
+          autoPlay
+          loop
+          muted
+        />
+        <div className="absolute w-full h-full bg-black opacity-70"></div>{" "}
+        {/* Darkened overlay */}
+        <div className="relative z-10 grid place-items-center overflow-hidden">
+          <img
+            className="w-16 h-16" // Apply animation to small logo
+            src={small}
+            alt="Small Abhyudaya Logo"
+          />
+          <img
+            className="w-3/4 h-auto animate-ease-out" // Animate white logo
+            src={heroLogoSrc}
+            alt="Abhyudaya Logo"
+          />
         </div>
+        <img
+          className="absolute right-5 top-5 w-16 h-16" // Apply animation to small logo
+          src={smallLogoSrc}
+          alt="MMMUT Logo"
+        />
       </div>
     </div>
   );
