0a6ce8a039dd6dde802e401257e3b4bc75400ad5
PRITISH TOMAR <pritishtomar2004@gmail.com>
2025-03-03 11:38:03 +0530

    playing with cors

--- Backend/index.js
+++ Backend/index.js
@@ -3,40 +3,43 @@ const eventRouter = require("./Routers/Events");
 const userRouter = require("./Routers/User");
 const VerificationRouter = require("./Routers/Verification");
 const { Connection } = require("./Connection");
-const { attachUserWithTokenVerification} = require("./authentication/UserAuth");
+const { attachUserWithTokenVerification } = require("./authentication/UserAuth");
 const PORT = process.env.PORT || 8000;
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { checkUser } = require("./authentication/Middleware");
 dotenv.config();
 
-Connection(); // for connecting DB
+Connection(); // Connecting DB
+
 const corsOptions = {
-  origin: process.env.APPLICATION_URL,
+  origin: "https://abhyudaya-25-nine.vercel.app",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-
-
-// app creation
+// App creation
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions)); // Use CORS globally
+app.options("*", cors(corsOptions)); // Handle preflight requests
+
 app.use(express.json());
 app.use(attachUserWithTokenVerification);
 
 // Ping request testing
 app.get("/", (req, res) => {
-  res.send("Ping from the server !");
+  res.setHeader("Access-Control-Allow-Origin", "https://abhyudaya-25-nine.vercel.app");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.send("Ping from the server!");
 });
 
 app.use("/events", eventRouter);
 app.use("/user", userRouter);
-app.use("/verify", checkUser,  VerificationRouter);
+app.use("/verify", checkUser, VerificationRouter);
 
-// ....................................................................
+// Start Server
 app.listen(PORT, () => {
   console.log("Server Running at Port " + PORT);
 });
