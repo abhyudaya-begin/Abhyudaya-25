00c3c9d45523f91cc5f9aa2fff3a67e2f58950c9
Chandan Gupta <cgupta7068@gmail.com>
2025-03-08 15:16:42 +0530

    added currentSponser/cg

--- Frontend/src/Components/homepage/Index-home.jsx
+++ Frontend/src/Components/homepage/Index-home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import Hero2Section from "./hero2";
 import HeroSection from "./hero";
-
 import ArtistSlider from "./slider/Slider";
 import CountDown from "./countdown/CountDown";
 import Merchandise from "./merchandise/Merchandise";
 import Stats from "./stats";
+import SponsorSlider from "./currentSponser/SponsorSlider"; // Import the new component
+
 const IndexHome = () => {
   return (
     <div
       className="flex flex-col select-none overflow-x-hidden gap-30 items-center bg-black text-white"
-      
     >
       <HeroSection />
       <Hero2Section /> 
+      <SponsorSlider /> {/* Move the new component */}
       <CountDown />
       {/* <ArtistSlider/> */}
       <Stats/>
       <ArtistSlider id="artists"/>
       <Merchandise/>
-     
-    
     </div>
   );
 };
