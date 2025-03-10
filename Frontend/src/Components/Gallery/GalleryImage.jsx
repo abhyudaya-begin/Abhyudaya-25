bf887ee07ea510749396814cd39a9441af121be3
PRITISH TOMAR <pritishtomar2004@gmail.com>
2025-03-10 22:10:52 +0530

    Images compressed-temas

--- Frontend/src/Components/Gallery/GalleryImage.jsx
+++ Frontend/src/Components/Gallery/GalleryImage.jsx
@@ -3,13 +3,13 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const GalleryImage = ({unique}) => {
-console.log(unique)
+
   return (
     <div className="relative rounded-lg overflow-hidden cursor-pointer select-none">
     <LazyLoadImage
       src={unique}
       alt="Gallery Image"
-      className="w-full h-auto"
+      className="w-full h-auto select-none"
       effect="blur"
       loading="lazy"
     />
