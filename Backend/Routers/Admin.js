9f03a532c5ce3d43573cede5037b89eae96a4c15
PRITISH TOMAR <pritishtomar2004@gmail.com>
2025-03-18 10:03:56 +0530

    Paymnet verifying / pritish

--- Backend/Routers/Admin.js
+++ Backend/Routers/Admin.js
@@ -6,25 +6,28 @@ const {
 } = require("../Controllers/Events");
 const { AdminLogin } = require("../Admin/Admin_Controller");
 const {Send, Verify} = require('../Admin/EmailForLogin');
-const { movePendingToPaid } = require("../Controllers/EventHandling");
+const { movePendingToPaid, getAllUserTransactions } = require("../Controllers/EventHandling");
+const { checkAdmin } = require("../authentication/Middleware");
 
 const adminRouter = Router();
 
 // Events manipulation
-adminRouter.post("events/", createEvent);
-adminRouter.put("events/:id", updateEvent);
-adminRouter.delete("events/:id", deleteEvent);
+// adminRouter.post("events/", checkAdmin, createEvent);
+// adminRouter.put("events/:id", updateEvent);
+// adminRouter.delete("events/:id", deleteEvent);
 
 // USer login and all
 adminRouter.post("/user", AdminLogin);
-adminRouter.post("/send-email", Send);
+adminRouter.post("/send-email",  Send);
 adminRouter.post("/verify-email", Verify);
 
 // Users per events.
 //
 
 //Payment Handlings and all
-adminRouter.post("/payment", movePendingToPaid);
+
+adminRouter.post("/payment", checkAdmin,  movePendingToPaid);
+adminRouter.get("/transactions", checkAdmin,  getAllUserTransactions);
 // 
 
 //Sponsors Upload.
